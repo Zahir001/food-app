858bdbe60ac847202343e9fb3f684bc1a28754dc
Zahir001 <mdzahir0123@gmail.com>
2026-05-22 16:12:39 +0530

    implement some middleware feature

--- mysite/settings.py
+++ mysite/settings.py
@@ -44,6 +44,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'myapp.middleware.BlockIPMiddleware',
+    'myapp.middleware.TimerMiddleware',
+    'myapp.middleware.LogRequestMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
