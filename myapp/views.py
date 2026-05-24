f54ddbbbd525e13b11288b9902f95f67dab14994
Zahir001 <mdzahir0123@gmail.com>
2026-05-24 18:08:06 +0530

    test debugging inbuilt django feature

--- myapp/views.py
+++ myapp/views.py
@@ -11,13 +11,22 @@ from django.urls import reverse_lazy
 from django.core.paginator import Paginator
 from django.views.decorators.cache import cache_page
 from django.views.decorators.vary import vary_on_headers
+import logging
+from django.shortcuts import get_object_or_404
+from django.utils import timezone
 
 # Create your views here.
 # @login_required
 # @cache_page(60 * 15)
 # @vary_on_headers("Agent-User") #Agent-User is case sensitive
+
+logger = logging.getLogger(__name__)
+
 def index(request):
+  logger.info("Fetching all items from the database")
+  logger.info(f"User{request.user} at {timezone.now()} time requested item list from {request.META.get('REMOTE_ADDR')} address")
   item_list = Item.objects.all()
+  logger.debug(f"found {item_list.count()} items")
   paginator = Paginator(item_list,5)
   page_number = request.GET.get('page') #request.GET se page no nika rha hia
   page_obj = paginator.get_page(page_number)
@@ -34,18 +43,25 @@ def index(request):
 #   context_object_name = 'item_list'
 
 
-# def detail(request, id):
-#   item = Item.objects.get(id=id)
-#   context = {
-#     "item":item
-#   }
-#   return render(request,"myapp/detail.html",context)
+def detail(request, id):
+  logger.info(f"Fetching an item with id:{id}")
+  try:
+      item = get_object_or_404(Item,pk=id) #isse item v milega aur error v check krega isiliye niche wala code comment kr diya
+      # item = Item.  objects.get(id=id)
+      logger.debug(f"Item found {item.item_name} $({item.item_price})")
+  except Exception as e:
+      logger.error(f"Error fetching the item with id {id},{e}")
+      raise
+  context = {
+    "item":item
+  }
+  return render(request,"myapp/detail.html",context)
   # return HttpResponse(f"This is detail view with id:{id}")
 
-class FoodDetailView(DetailView):
-  model = Item
-  template_name = 'myapp/detail.html'
-  context_object_name = 'item'
+# class FoodDetailView(DetailView):
+#   model = Item
+#   template_name = 'myapp/detail.html'
+#   context_object_name = 'item'
   
 
 
